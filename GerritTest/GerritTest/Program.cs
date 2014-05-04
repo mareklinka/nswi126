36e9ff49284c19185574fe44fde8e580a9f38605
Marek Linka <marek.linka@elitecode.cz>
2014-05-04 11:48:44 +0200

    Skeleton code

---
+++ GerritTest/GerritTest/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GerritTest
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Implement sanity checks
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+
+            // this right here is obviously a very complicated calculation
+            // better make someone else verify correctness
+            // TODO: Implement discriminant
+        }
+    }
+}
